55330e964f493fe261b7d45a3254e530483699ea
Peter Upfold <Peter@orion.local>
2018-11-09 06:59:02 +0000

    Initial Commit

---
+++ DfontSplitter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DfontSplitter
+//
+//  Created by Peter Upfold on 09/11/2018.
+//  Copyright © 2018 Peter Upfold. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DfontSplitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DfontSplitter
+//
+//  Created by Peter Upfold on 09/11/2018.
+//  Copyright © 2018 Peter Upfold. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
