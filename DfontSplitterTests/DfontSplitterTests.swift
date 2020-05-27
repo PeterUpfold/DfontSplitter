1393ccfac2c40dcf237ae5991873790816c3ddc1
Peter Upfold <pgithub@upfold.org.uk>
2020-05-27 20:19:44 +0100

    Add PTSans tests

--- DfontSplitterTests/DfontSplitterTests.swift
+++ DfontSplitterTests/DfontSplitterTests.swift
@@ -7,11 +7,13 @@
 //
 
 import XCTest
-import CommonCrypto
+import os
 @testable import DfontSplitter
 
 // Before the test will work, the SampleFiles/ directory must be copied to
 // ~/Library/Containers/uk.org.upfold.DfontSplitter/Data/SampleFiles
+
+// some tests may require fonts that cannot be redistributed in the open source repository
 
 class DfontSplitterTests: XCTestCase {
     
@@ -26,6 +28,19 @@ class DfontSplitterTests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        os_log("Cleaning temp folder")
+        
+        let enumerator = FileManager.default.enumerator(atPath: NSTemporaryDirectory())
+        
+        while let file = enumerator?.nextObject() as? String {
+            os_log("clean %s", URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file).absoluteString)
+            do {
+                try FileManager.default.removeItem(at: URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file))
+            }
+            catch {
+                os_log("failed to remove %s", URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file).absoluteString)
+            }
+        }
     }
 
     func testExample() {
@@ -34,16 +49,17 @@ class DfontSplitterTests: XCTestCase {
     }
     
     func testPTSansOTF() {
-        
-
+    
         XCTAssertEqual(runFonduOnRelativePath(path: "SampleFiles/PTSans-Regular.otf.dfont", currentURL: currentURL),
                        1,
                        "fondu did not return 1")
 
         // we are expecting files in the output folder -- PTSans.otf and PTSansItalic.otf
+        let ptSansOTFPath = tempURL.appendingPathComponent("PTSans.otf")
+        let ptSansItalicOTFPath = tempURL.appendingPathComponent("PTSansItalic.otf")
         
-        
-        
+        XCTAssertEqual(getSHA256HexString(url: ptSansOTFPath), "2101fe4abb8b97da7ec339afa93d4c3575d01a6498abc306e7349d811c128747", "PTSans.otf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansItalicOTFPath), "b84e4f1c297414a4d41d4d555da457d13f98a3a11b5e1612265ef15f9fc9a7c3", "PTSansItalic.otf did not match expected")
     }
 
     func testPerformanceExample() {
--- DfontSplitterTests/Utils.swift
+++ DfontSplitterTests/Utils.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import DfontSplitter
+import CommonCrypto
 
 func runFonduOnRelativePath(path: String, currentURL: URL) -> Int {
     let file = currentURL.appendingPathComponent(path).path
@@ -16,4 +17,28 @@ func runFonduOnRelativePath(path: String, currentURL: URL) -> Int {
     let unsafeMutablePointerOfFilename: UnsafeMutablePointer<Int8> = UnsafeMutablePointer<Int8>(mutating: unsafePointerOfFilename!)
     
     return Int(fondu_main_simple(unsafeMutablePointerOfFilename))
+}
+
+func getSHA256Hash(url: URL) -> NSData {
+    let data = FileManager.default.contents(atPath: url.path)
+    
+    let bytes = [UInt8](data!)
+    
+    var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+    CC_SHA256(bytes, CC_LONG(bytes.count), &hash)
+    
+    let result = NSData(bytes: hash, length: Int(CC_SHA256_DIGEST_LENGTH))
+    return result
+}
+
+func getSHA256HexString(url: URL) -> String {
+    let data = getSHA256Hash(url: url)
+    var bytes = [UInt8](repeating:0, count:data.count)
+    data.getBytes(&bytes, length: data.count)
+    
+    var output = ""
+    for byte in bytes {
+        output += String(format:"%02x", UInt8(byte))
+    }
+    return output
 }
