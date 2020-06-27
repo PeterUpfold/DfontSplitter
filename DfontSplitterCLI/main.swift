6e100d4cb065d8cb6a22f32dbb846e17dd295947
Peter Upfold <pgithub@upfold.org.uk>
2020-06-27 14:59:14 +0100

    CLI wrapper builds

--- DfontSplitterCLI/main.swift
+++ DfontSplitterCLI/main.swift
@@ -5,8 +5,39 @@
 //  Created by Peter Upfold on 25/06/2020.
 //  Copyright © 2020 Peter Upfold. All rights reserved.
 //
+/*
+ This file is part of DfontSplitter.
+ 
+ DfontSplitter is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ DfontSplitter is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with DfontSplitter.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import Foundation
+import DfontSplitter
+import CommonCrypto
 
-print("Hello, World!")
+func runFonduOnRelativePath(path: String, currentURL: URL) -> Int {
+    let file = currentURL.appendingPathComponent(path).path
+    
+    let unsafePointerOfFilename = NSString(string: file).utf8String
+    let unsafeMutablePointerOfFilename: UnsafeMutablePointer<Int8> = UnsafeMutablePointer<Int8>(mutating: unsafePointerOfFilename!)
+    
+    return Int(fondu_main_simple(unsafeMutablePointerOfFilename))
+}
+
+
+let arguments = CommandLine.arguments
+
+runFonduOnRelativePath(path: arguments[1], currentURL: URL(fileURLWithPath: FileManager.default.currentDirectoryPath))
+
 
