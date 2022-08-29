5fad491be4c89d943c81a25b265a1bdfef10164d
Peter Upfold <pgithub@upfold.org.uk>
2022-08-29 14:35:56 +0100

    Add Font Suitcase tests

--- DfontSplitterTests/DfontSplitterTests.swift
+++ DfontSplitterTests/DfontSplitterTests.swift
@@ -61,6 +61,43 @@ class DfontSplitterTests: XCTestCase {
         XCTAssertEqual(getSHA256HexString(url: ptSansOTFPath), "2101fe4abb8b97da7ec339afa93d4c3575d01a6498abc306e7349d811c128747", "PTSans.otf did not match expected")
         XCTAssertEqual(getSHA256HexString(url: ptSansItalicOTFPath), "b84e4f1c297414a4d41d4d555da457d13f98a3a11b5e1612265ef15f9fc9a7c3", "PTSansItalic.otf did not match expected")
     }
+    
+    func testPTSansSuitcase() {
+        // extract the zip -- we must do this with macOS native tools that will extract the rsrc fork properly.
+        // SampleFiles/PTSansFontSuitcase.zip
+        let task = Process()
+        task.launchPath = "/usr/bin/ditto"
+        task.arguments = ["-x", "-k", "SampleFiles/PTSansFontSuitcase.zip", "SampleFiles"]
+        task.currentDirectoryURL = currentURL
+        task.launch()
+        task.waitUntilExit()
+        
+        XCTAssertEqual(task.terminationStatus, 0, "ditto did not successfully extract the Font Suitcase zip")
+        
+        XCTAssertEqual(runFonduOnRelativePath(path: "SampleFiles/PTSans", currentURL: currentURL),
+                       1,
+                       "fondu did not return 1")
+        
+        // we are expecting files in the output folder -- PTSans.ttf, PTSansBold.ttf, PTSansBoldItalic.ttf, PTSansCaption.ttf, PTSansCaptionBold.ttf, PTSansItalic.ttf, PTSansNarrow.ttf, PTSansNarrowBold.ttf
+        
+        let ptSansTTFPath = tempURL.appendingPathComponent("PTSans.ttf")
+        let ptSansBoldTTFPath = tempURL.appendingPathComponent("PTSansBold.ttf")
+        let ptSansBoldItalicTTFPath = tempURL.appendingPathComponent("PTSansBoldItalic.ttf")
+        let ptSansCaptionPath = tempURL.appendingPathComponent("PTSansCaption.ttf")
+        let ptSansCaptionBoldPath = tempURL.appendingPathComponent("PTSansCaptionBold.ttf")
+        let ptSansItalicPath = tempURL.appendingPathComponent("PTSansItalic.ttf")
+        let ptSansNarrowTTFPath = tempURL.appendingPathComponent("PTSansNarrow.ttf")
+        let ptSansNarrowBoldTTFPath = tempURL.appendingPathComponent("PTSansNarrowBold.ttf")
+        
+        XCTAssertEqual(getSHA256HexString(url: ptSansTTFPath), "bd39569d17eea12d4c9c5390da1ab519184328354f9ef75db7f5e6011e3d396e", "PTSans.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansBoldTTFPath), "162d387a347b6e6c81b5439731c7075ea64f1185a169fe831e02577fc9bb5adb", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansBoldItalicTTFPath), "fe0292b98440fcfe7f942fb83f79562b750330a1efa85809bb8cde57c7442320", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansCaptionPath), "e0eceda826f3147024f8b40db4da6094c09140371508eb889323d04ef657a9fb", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansCaptionBoldPath), "05f2b654f6cc04059150fc24625f856e3c71364d23374131e9a4a4998e27e0c2", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansItalicPath), "f641597430bae9b5151f0595c7f5aa06f61bc9f19b31dd0a59bc29a787dea474", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansNarrowTTFPath), "bf6dc13c29d8431e704eb2247abd7c96f56451cfb2db2bd5615435cf53583225", "PTSansBold.ttf did not match expected")
+        XCTAssertEqual(getSHA256HexString(url: ptSansNarrowBoldTTFPath), "97fe6d9726c2466b46f1dac07c2b4d0aaa3f160b0319f9117c63bdb2048888db", "PTSansBold.ttf did not match expected")
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
